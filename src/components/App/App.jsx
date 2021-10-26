ec252a76dea0df2cc34d672dd6145005fa82cae4
Lucas Ribeiro <lucasrb04@gmail.com>
2021-10-26 18:54:51 -0300

    Adicionando eslint e stylelint, criando camada de service, para a requisiçao no API, formatando os dados para enviar para api e começando tratamento de erros

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,37 +3,44 @@ import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
 import CalcForm from '../CalcForm/CalcForm';
 import PaintCans from '../PaintCans/PaintCans';
+import api from '../../services/calculateApi';
 
 import { getData, storeData } from '../../helpers/localStorage';
 
 const App = () => {
-  const initialState = () => getData('data') || [];
-  const [state, setState] = useState(initialState);
-  const [data, setData] = useState({});
+  // const initialState = () => getData('data') || [];
+  const [cansList, setCansList] = useState({});
 
-  useEffect(() => {
-    storeData('data', state);
-    const date = state.map(obj => obj.date);
-    const bmi = state.map(obj => obj.bmi);
-    let newData = { date, bmi };
-    setData(newData);
-  }, [state]);
+  // useEffect(() => {
+  //   storeData('data', state);
+  //   const date = state.map((obj) => obj.date);
+  //   const bmi = state.map((obj) => obj.bmi);
+  //   const newData = { date, bmi };
+  //   setData(newData);
+  // }, [state]);
+
+  const handleChange = async (roomObjInfo) => {
+    const paintCans = await api(roomObjInfo);
+    setCansList(paintCans);
+  };
+
+  // useEffect(() => {
+  //   console.log('roomInfo', roomInfo);
+  // }, [roomInfo]);
 
   return (
-    <div className='container'>
-      <div className='row center'>
-        <h1 className='white-text'> Paint Calculator </h1>
+    <div className="container">
+      <div className="row center">
+        <h1 className="white-text"> Paint Calculator </h1>
       </div>
-      <div className='row'>
-        <div className='col m12 s12'>
-          <CalcForm />
-          <div>
-            <div className='row center'>
-              <h4 className='white-text'>Pai</h4>
-            </div>
-            <div className='data-container row'>
-              { state.length > 0 && <PaintCans /> }
-            </div>
+      <div className="row">
+        <div className="col m12 s12">
+          <CalcForm change={ handleChange } />
+          <div className="row center">
+            <h4 className="white-text">Pai</h4>
+          </div>
+          <div className="data-container row">
+            { cansList !== {} && <PaintCans cans={ cansList } /> }
           </div>
         </div>
       </div>
--- src/components/CalcForm/CalcForm.jsx
+++ src/components/CalcForm/CalcForm.jsx
@@ -1,92 +1,129 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import '../App/App.css';
 
 const initialValues = {
-	wall0: {
-		width0: '',
-		height0: '',
-	},
-	wall1: {
-		width1: '',
-		height1: '',
-	},
-	wall2: {
-		width1: '',
-		height1: '',
-	},
-	wall3: {
-		width1: '',
-		height1: '',
-	},
-}
+  wall0: {
+    width0: 3,
+    height0: 3,
+  },
+  wall1: {
+    width1: 3,
+    height1: 3,
+  },
+  wall2: {
+    width2: 3,
+    height2: 3,
+  },
+  wall3: {
+    width3: 3,
+    height3: 3,
+  },
+};
 
 const CalcForm = ({ change }) => {
-	const [state, setState] = useState(initialValues);
+  const [state, setState] = useState(initialValues);
 
-	const handleChange = (e) => {
-		let { value, name, id } = e.target;
-		setState({
-			...state,
-			[name]: {
-				...state[name],
-				[id]: value
-			},
-		});
-	};
+  const handleChangeWall = (e) => {
+    const { value, name, id } = e.target;
+    // função que atualiza o estado
+    // a partir do id e do name do input,
+    // com o id ele decide se altera o objeto height ou width
+    // e com o campo name ele defina qual parede está editando.
+    // o Radix(+) é para converter o valor em número
+    setState({
+      ...state,
+      [name]: {
+        ...state[name],
+        [id]: parseFloat(value),
+      },
+    });
+  };
 
-	const handleSubmit = () => {
-		// change(state);
-		console.log(initialValues);
-		setState(initialValues);
-	};
+  const formatData = (data) => {
+    const formatedData = {
+      walls: [
+        {
+          width: data.wall0.width0,
+          height: data.wall0.height0,
+        },
+        {
+          width: data.wall1.width1,
+          height: data.wall1.height1,
+        },
+        {
+          width: data.wall2.width2,
+          height: data.wall2.height2,
+        },
+        {
+          width: data.wall3.width3,
+          height: data.wall3.height3,
+        },
+      ],
+    };
+    return formatedData;
+  };
 
-	return (
-		<>
-		{ Object.keys(state).map((wall, idx) => (
-						<div className="row">
-						<div className="col m6 s12">
-							<label htmlFor={`width${idx}`}>Width (in meters)</label>
-							<input
-								id={`width${idx}`}
-								name={wall}
-								prop='width'
-								type="number"
-								min="1"
-								max="15"
-								placeholder="Type the width"
-								value={state[wall].width}
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="col m6 s12">
-							<label htmlFor="height">Height (in meters)</label>
-							<input
-								id={`height${idx}`}
-								name={`${wall}.height`}
-								type="number"
-								min="2.2"
-								max="999"
-								placeholder="Type the height"
-								value={state[wall].height}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-		))}
+  const handleSubmit = () => {
+    const data = formatData(state);
+    change(data);
+  };
 
-			<div className="center">
-				<button
-					id="calc-btn"
-					className="calculate-btn"
-					type="button"
-					// disabled={state.weight === '' || state.height === ''}
-					onClick={handleSubmit}
-				>
-					Calculate PaintCans
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      { Object.keys(state).map((wall, idx) => (
+        <div key={ wall } className="row">
+          <div className="col m6 s12">
+            <label htmlFor={ `width${idx}` }>
+              Width (in meters)
+              <input
+                id={ `width${idx}` }
+                name={ wall }
+                prop="width"
+                type="number"
+                min="1"
+                max="15"
+                placeholder="Type the width"
+                value={ state[wall].width }
+                onChange={ handleChangeWall }
+              />
+            </label>
+
+          </div>
+          <div className="col m6 s12">
+            <label htmlFor={ `height${idx}` }>
+              Height (in meters)
+              <input
+                id={ `height${idx}` }
+                name={ wall }
+                type="number"
+                min="2.2"
+                placeholder="Type the height"
+                value={ state[wall].height }
+                onChange={ handleChangeWall }
+              />
+            </label>
+          </div>
+        </div>
+      ))}
+
+      <div className="center">
+        <button
+          id="calc-btn"
+          className="calculate-btn"
+          type="button"
+          // disabled={state.weight === '' || state.height === ''}
+          onClick={ handleSubmit }
+        >
+        Calculate PaintCans
+        </button>
+      </div>
+    </>
+  );
+};
+
+CalcForm.propTypes = {
+  change: PropTypes.func.isRequired,
 };
 
 export default CalcForm;
